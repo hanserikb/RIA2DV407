4d5a7dbe2028a5ad953ea8236a885174489ac573
Hans Bentlöv <hanserikb@gmail.com>
2013-02-20 17:23:59 +0100

    Add comments

--- src/views/dish.js
+++ src/views/dish.js
@@ -1,14 +1,18 @@
 define(['backbone', 'handlebars', 'text!templates/dish.html'], function(Backbone, Handlebars, DishTemplate) {
   return Backbone.View.extend({
     tagName: 'div',
+    // Using handlebars to compile templates
     template: Handlebars.compile($(DishTemplate).html()),
 
     initialize: function() {
+      // Reacts on changes
       this.on('change', function() {
         console.log('dish just changed!');
       });
     },
 
+    // Renders a single dish into a div using the given model object
+    // Also returns the complete view
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       return this;
