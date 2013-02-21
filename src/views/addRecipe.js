c12c28c00c4edeedcc4f0916494b90e8b17e41f1
Hans Bentlöv <hanserikb@gmail.com>
2013-02-21 23:21:38 +0100

    AddRecipe tests, template, view

--- src/views/addRecipe.js
+++ src/views/addRecipe.js
@@ -1,5 +1,53 @@
-define(['backbone', 'handlebars'], function(Backbone, Handlebars) {
+define(['jquery', 'underscore', 'backbone', 'handlebars', 'text!templates/addRecipe.html', '../models/dish'], function($, _, Backbone, Handlebars, addRecipeTemplate, DishModel) {
   return Backbone.View.extend({
+    template: Handlebars.compile(addRecipeTemplate),
+    formData: {},
 
+    initialize: function(opts) {
+      this.model = new DishModel();
+      this.render();
+        // Listen to model validation errors
+      this.listenTo(this.model, 'invalid', this.renderErrors);
+      this.listenTo(this.model, 'change', function(model){ 
+           if (model.isValid) {
+            $('#errors').html('');
+           };
+        })
+    },
+
+    // Render the errors and append them to a error-container
+    renderErrors: function(model) {
+      console.log('rendererrors');
+      this.$('#errors').html('');
+      _.each(model.validationError, function(error) {
+        // TODO: Render the messages in HTML
+        $('#errors').append(error.message + '<br>');
+      });
+    },
+
+    events: {
+      // Validate model on every keyup
+      'keyup input': 'inputChange',
+      'keyup textarea': 'inputChange'
+    },
+
+    // Sets the model to the typed value
+    inputChange: function(e) {
+      // Gets the field the user is typing in
+      var field = $(e.currentTarget);
+
+      // Populate the formData object with correct values
+      // Gets the model attribute name from the form input-field ID
+      // (string-dependency == bad??)
+      this.formData[field.attr('id')] = field.val();
+
+      // Sets values on the recipe model object
+      this.model.set(this.formData, {validate: true});
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    }
   });
 });
---
+++ test/spec/views/addRecipe.js
@@ -0,0 +1,36 @@
+define(['../../../src/views/addRecipe.js', '../../../src/models/dish.js'], function(AddRecipeView, DishModel) {
+  // ## Test suite for the addRecipe view
+  describe('the initialize function', function() {
+      var initialize = AddRecipeView.prototype.initialize,
+        context = {
+          listenTo: jasmine.createSpy('listenToSpy').andReturn(context),
+          render: jasmine.createSpy('renderSpy').andReturn(context)
+      };
+      initialize.call(context);
+
+      it('called listenTo correctly', function() {
+        expect(context.listenTo).toHaveBeenCalled();
+        expect(context.listenTo.callCount).toBe(2);
+      });
+
+      it('called render', function() {
+        expect(context.render).toHaveBeenCalled();
+        expect(context.render.callCount).toBe(1);
+      });
+
+  });
+
+
+  describe('The addRecipe view', function() {
+
+    addRecipeView = null;
+
+    beforeEach(function() {
+      addRecipeView = new AddRecipeView({model: new DishModel()});
+    });
+
+    it('should have a dish model connected to it', function() {
+      expect(addRecipeView.model).toBeDefined();
+    });
+  });
+});
