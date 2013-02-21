cd751f6af84b971d01643384acd06c9be2e0b9a6
Hans Bentlöv <hanserikb@gmail.com>
2013-02-21 23:29:01 +0100

    Add functionality to show a 'newdish'-view

--- src/views/app.js
+++ src/views/app.js
@@ -1,12 +1,36 @@
-define(['jquery', 'backbone', 'underscore', '../../../src/views/dish.js', '../../../src/models/dish.js', '../views/recipeList', '../collections/dishes'], function($, Backbone, _, DishView, DishModel, recipeList, DishCollection) {
-  var Appview = Backbone.View.extend({
-    $el: $('.container'),
+define(['jquery',
+  'backbone',
+  'underscore',
+  '../../../src/views/dish.js',
+  '../../../src/models/dish.js',
+  '../views/recipeList',
+  '../collections/dishes',
+  '../views/addRecipe',
+  'Bootstrap-modal'], function($, Backbone, _, DishView, DishModel, recipeList, DishCollection, addRecipeView) {
+    var Appview = Backbone.View.extend({
+      el: 'body',
+      $el: $('body'),
 
-    initialize: function() {
-      //recipeCollection = new DishCollection();
-      recipes = new recipeList();
-    }
+      initialize: function() {
+        recipeCollection = new DishCollection();
+        recipes = new recipeList({collection: recipeCollection});
+      },
 
+      events: {
+        'click #add-recipe': "newAddRecipeView"
+      },
+
+      newAddRecipeView: function() {
+        var addRecipeview = new addRecipeView();
+        this.$el.append(addRecipeview.render().el);
+        $('#add-recipe-modal').modal().css({
+          width: 'auto',
+          'margin-left': function () {
+            return -($(this).width() / 2);
+          }
+        });;
+      }
+
+    });
+    return Appview;
   });
-  return Appview;
-});
