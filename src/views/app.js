8a248f6babbf7f6eaf67e83fa9e61b397c2f9946
Hans Bentlöv <hanserikb@gmail.com>
2013-02-21 11:01:35 +0100

    Smallfix

--- src/views/app.js
+++ src/views/app.js
@@ -1,11 +1,10 @@
 define(['jquery', 'backbone', 'underscore', '../../../src/views/dish.js', '../../../src/models/dish.js', '../views/recipeList', '../collections/dishes'], function($, Backbone, _, DishView, DishModel, recipeList, DishCollection) {
-  window.DishModel = DishModel;
   var Appview = Backbone.View.extend({
     $el: $('.container'),
 
     initialize: function() {
-      //window.recipeCollection = new DishCollection();
-      window.recipes = new recipeList({collection: window.recipeCollection});
+      //recipeCollection = new DishCollection();
+      recipes = new recipeList();
     }
 
   });
