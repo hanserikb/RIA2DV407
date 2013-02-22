9630b8e15aeec98bea8bb182abf84be172958b22
Hans Bentlöv <hanserikb@gmail.com>
2013-02-22 10:08:29 +0100

    Remove backbone from global scope

--- src/config.js
+++ src/config.js
@@ -5,16 +5,28 @@ require.config({
     'jquery': '../lib/jquery/jquery',
     'underscore': '../lib/underscore-amd/underscore',
     'handlebars': '../lib/handlebars.js/dist/handlebars',
-    'backbone': '../lib/backbone/backbone',
+    'pureBackbone': '../lib/backbone/backbone',    
+    'backbone': '../lib/backbone/backbone.module',
     'backbone-relational': '../lib/backbone-relational/backbone-relational',
     'localstorage': '../lib/backbone.localStorage/backbone.localStorage',
-    'bootstrap-modal-jakobmattsson': '../lib/bootstrap-modal-jakobmattsson/js/bootstrap-modal',
     'jasmine': '../lib/jasmine/lib/jasmine-core/jasmine',
     'jasmine-jquery': '../lib/jasmine-jquery/lib/jasmine-jquery',
     'jasmine-html': '../lib/jasmine/lib/jasmine-core/jasmine-html',
-    'Bootstrap-modal': '../lib/bootstrap/js/bootstrap-modal',
+    'Bootstrap-modal': '../lib/bootstrap/js/bootstrap-modal'
   },
   shim: {
+    jquery: {
+      init: function() {
+        return this.jQuery.noConflict(true);
+      }
+    },
+    backbone: {
+      deps: ['pureBackbone'],
+    },
+    pureBackbone: {
+      deps: ['jquery', 'underscore'],
+      exports: 'Backbone'
+    },
     jasmine: {
       exports: 'jasmine'
     },
@@ -22,27 +34,17 @@ require.config({
       deps: ['jasmine'],
       exports: 'jasmine'
     },
-    backbone: {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    },
     'backbone-relational': {
-      deps: ['backbone'],
-      exports: 'Backbone'
+      deps: ['underscore', 'pureBackbone']
     },
     'localstorage': {
-      deps: ['backbone'],
-      exports: 'Backbone'
-    },
-    'bootstrap-modal-jakobmattsson': {
-      deps: ['backbone'],
-      exports: 'Backbone'
+      deps: ['underscore', 'pureBackbone']
     },
     'handlebars': {
-      exports: 'Handlebars' 
+      exports: 'Handlebars'
     },
     'Bootstrap-modal': {
-      exports: 'Bootstrap-modal',
+      exports: 'Bootstrap-modal'
     }
   }
 });
@@ -52,10 +54,10 @@ if (window.TEST) {
   require(['../test/config']);
 } else {
   require(
-  ['views/app', 'router'], function(AppView, Router) {
-    new Router();
-    Backbone.history.start();
+    ['views/app', 'router', 'pureBackbone'], function(AppView, Router, Backbone) {
+      new Router();
+      Backbone.history.start();
 
-    window.appView = new AppView();
-  });
+      window.appView = new AppView();
+    });
 }
